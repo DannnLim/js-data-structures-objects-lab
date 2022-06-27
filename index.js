09076765979acd1d459117b8293dc3bf2db03407
DannnLim <Dannylim-@hotmail.com>
2022-06-27 18:02:50 +1000

    commit tree b39feedf3989ed85d62fb12eefc740acd9bbd15d

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution in this file!
+
+const driver = {
+    name: "Sam",
+    address: "11 Broadway"
+};
+
+function updateDriverWithKeyAndValue(driver, name, address) {
+    const newUpdate = { ...driver };
+    newUpdate[name] = address;
+    return newUpdate
+}
+console.log(updateDriverWithKeyAndValue.newUpdate)
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, name, address) {
+    driver[name] = address;
+    return driver
+}
+
+console.log(driver)
+
+function deleteFromDriverByKey(newUpdate, name) {
+    delete { ...newUpdate };
+    return {}
+
+}
+console.log(deleteFromDriverByKey())
+console.log(driver)
+
+function destructivelyDeleteFromDriverByKey(driver, name) {
+    delete driver.name;
+    return driver
+}
+
+console.log(destructivelyUpdateDriverWithKeyAndValue(driver))
